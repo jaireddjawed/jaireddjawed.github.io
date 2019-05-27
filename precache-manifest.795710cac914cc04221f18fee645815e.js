0395f70fdbf8167c65ce9e828d21a8eb0360fbbe
Jaired Jawed <jairedjawed@icloud.com>
2019-05-26 23:52:07 -0700

    Updates

---
+++ precache-manifest.795710cac914cc04221f18fee645815e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbea587b416865c07b540215c2dda387",
+    "url": "/jaireddjawed.github.io/index.html"
+  },
+  {
+    "revision": "ea5781701c1eb509a1f5",
+    "url": "/jaireddjawed.github.io/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/jaireddjawed.github.io/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ea5781701c1eb509a1f5",
+    "url": "/jaireddjawed.github.io/static/js/main.39890843.chunk.js"
+  },
+  {
+    "revision": "d539ebeb90a2bf5c395e",
+    "url": "/jaireddjawed.github.io/static/js/runtime~main.975377c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jaireddjawed.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
