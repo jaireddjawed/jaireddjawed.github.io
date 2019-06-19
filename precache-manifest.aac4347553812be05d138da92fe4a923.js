50720ab912b0cf9b839ccd7b248a9258da56b40b
Jaired Jawed <jairedjawed@icloud.com>
2019-06-19 16:21:58 -0700

    Updates

--- precache-manifest.aac4347553812be05d138da92fe4a923.js
+++ precache-manifest.aac4347553812be05d138da92fe4a923.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17d7a44d5818dd1c75793f8322f8895e",
+    "revision": "1e34a9dd48d09656d6ac8df1772d0d0c",
     "url": "/jaireddjawed.github.io/index.html"
   },
   {
-    "revision": "e1889e340086b61ad8df",
+    "revision": "2b010ca56df7a8c87355",
     "url": "/jaireddjawed.github.io/static/css/main.b388d55f.chunk.css"
   },
   {
-    "revision": "2e0129063922e3bb197a",
-    "url": "/jaireddjawed.github.io/static/js/2.b41502e9.chunk.js"
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "/jaireddjawed.github.io/static/js/2.7ad91dd1.chunk.js"
   },
   {
-    "revision": "e1889e340086b61ad8df",
-    "url": "/jaireddjawed.github.io/static/js/main.39a1fa7e.chunk.js"
+    "revision": "2b010ca56df7a8c87355",
+    "url": "/jaireddjawed.github.io/static/js/main.3bd0aca5.chunk.js"
   },
   {
     "revision": "d539ebeb90a2bf5c395e",
@@ -22,5 +22,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "dba8ec50bddb1ab024fea31ea6d45176",
     "url": "/jaireddjawed.github.io/static/media/profile.dba8ec50.jpeg"
+  },
+  {
+    "revision": "94f8eef8b0ae459b7bd2b6acabb6bf90",
+    "url": "/jaireddjawed.github.io/static/media/sticks-and-stones.94f8eef8.png"
   }
 ]);
