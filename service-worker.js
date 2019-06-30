09711888cefd64e66eab43aafc735725e26c6be9
Jaired Jawed <jairedjawed@icloud.com>
2019-06-30 01:29:05 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jaireddjawed.github.io/precache-manifest.ed97f90a837ee17875e914eb5d1b82a7.js"
+  "/jaireddjawed.github.io/precache-manifest.06bb18ddebe50b2b2b7e490a37ac9d70.js"
 );
 
 self.addEventListener('message', (event) => {
